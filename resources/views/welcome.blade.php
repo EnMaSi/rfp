9beb8232db68eaf8a77d392a841ed2ff598add04
Klaus Harm <Klaus.Harm@enmasi.com>
2019-11-14 22:31:41 +0100

    Image op homepagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-Wij vinden het prachtig
+                    <img src="{{url('https://enmasi.com/EnMaSi_landing.png')}}" alt="EnMaSi"/>
                 </div>
 
                 <div class="links">
